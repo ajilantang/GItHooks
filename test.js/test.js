aabb2d0dce977fbf5d791557d7113fe5c5c4264f
ajilantang <ajilantang@gmail.com>
2017-07-27 15:47:09 +0700

    asa

--- test.js/test.js
+++ test.js/test.js
@@ -5,6 +5,6 @@ describe("Person", function(){
     it("greets people", function(){
         var p1 = new Person("Ajis"),
             p2 = new Person("Andy");
-        p1.greet(p2).should.equal("Hello, Anasdasdy!")
+        p1.greet(p2).should.equal("Hello, Andy!")
     })
 })
